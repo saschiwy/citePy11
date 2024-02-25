88dcf18c9fddab4f3ebf68c170d1a55d35f184d1
Sascha Schiwy <sascha.schiwy@gmail.com>
2024-02-25 12:45:12 +0100

    Added some examples

--- test_mapped_library.py
+++ test_mapped_library.py
@@ -1,11 +1,6 @@
 import citePyExample.citePyExample as m
 
-example_struct = m.CitePyExampleNS.ExampleStruct(5, 3)
 example_lib = m.CitePyExampleNS.IExample.createLibrary()
-
-example_struct.left = 6
-example_struct.right = 10
-
 
 def cb(value):
     print(f'Callback: {value}')
@@ -16,9 +11,13 @@ external_struct = m.SecondNamespace.ExternalStruct()
 external_struct.listWithNumbersToAdd = [1, 2]
 print('External Struct, listWithNumbersToAdd: ' + str(external_struct.listWithNumbersToAdd))
 
-operation = m.CitePyExampleNS.ExampleEnum.subtract
-example_struct = m.CitePyExampleNS.ExampleStruct(5, 10)
+operation = m.CitePyExampleNS.ExampleEnum.add
+example_struct = m.CitePyExampleNS.ExampleStruct()
+example_struct.left = 5
+example_struct.right = 10
 res = example_lib.compute(operation, example_struct)
 print(f'ExampleLib.compute(Enum, Example Struct: {5} + {10} = {res}')
-print(f'ExampleLib.compute(External Struct: {1} + {2} = {example_lib.compute(external_struct)}')
+
+res = example_lib.compute(external_struct)
+print(f'ExampleLib.compute(External Struct: {1} + {2} = {res}')
 # print(f'ExampleLib.compute(List: {1.1} + {5.1} = {example_lib.compute([1.1, 5.1])}')
